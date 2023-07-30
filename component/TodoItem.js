7697bde9f0571058c67cdeb98c56ccc3f7b22b02
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2023-07-30 20:13:40 +0700

    New

--- component/TodoItem.js
+++ component/TodoItem.js
@@ -4,11 +4,11 @@ function TodoItem({ todo }) {
     return html `
         <li class="${todo.completed && 'completed'}">
             <div class="view">
-                <input class="toggle" type="checkbox" checked>
+                <input class="toggle" type="checkbox" ${todo.completed && 'checked'}>
                 <label>${todo.title}</label>
                 <button class="destroy"></button>
             </div>
-            <input class="edit" value="Create a TodoMVC template">
+            <input class="edit" value="${todo.title}">
         </li>
     `
 }
